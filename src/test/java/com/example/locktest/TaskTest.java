e9381e5584e83d80325977166d6df5974d2d246c
rafal-nowak <contact@rafal-nowak.com>
2022-05-12 12:45:49 +0200

    refactor

--- src/test/java/com/example/locktest/TaskTest.java
+++ src/test/java/com/example/locktest/TaskTest.java
@@ -20,13 +20,17 @@ class TaskTest {
     }
 
     @Test
-    void saved_entity_should_have_id() {
+    void persisted_entity_should_have_id() {
         var task = Fixtures.testTask();
         System.out.println("##########");
         UUID taskIdBeforePersist = task.getId();
-        System.out.println("task id before save: " + taskIdBeforePersist);
+        System.out.println("task id before persist: " + taskIdBeforePersist);
         Task savedTask = taskRepository.save(task);
+        System.out.println("##########");
+        UUID taskIdAfterPersist = task.getId();
+        System.out.println("task id after persist: " + taskIdAfterPersist);
         assertNotEquals(taskIdBeforePersist, savedTask.getId());
+        assertNotNull(task.getId());
     }
 
 }
